5e33c816d7a7dec9931d25c5521a5c38f07c6632
VALE <hunterdemon2202@gmail.com>
2025-10-14 08:19:35 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,10 @@
-add_lunch_combo omni_X6739-user
-add_lunch_combo omni_X6739-userdebug
-add_lunch_combo omni_X6739-eng
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_X6820-user
+add_lunch_combo omni_X6820-userdebug
+add_lunch_combo omni_X6820-eng
