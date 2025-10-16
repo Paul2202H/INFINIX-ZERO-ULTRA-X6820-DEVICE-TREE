e52a9316651c597c3ff6af30870dfcbaf09afaea
VALE <hunterdemon2202@gmail.com>
2025-10-16 20:58:17 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -38,7 +38,6 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 
     # Version / Maintainer infos.
 	export OF_MAINTAINER="VALE"
-	export FOX_VERSION="R11.3"
 	export FOX_BUILD_TYPE="Unofficial-Stable"
 
     # Device Information.
